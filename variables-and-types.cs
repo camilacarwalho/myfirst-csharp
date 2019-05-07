595766323052f629bc879b2602186551515ae1a4
Camila Carwalho <camilacarwalho3@gmail.com>
2019-05-07 16:53:07 -0300

    Variáveis e tipos

---
+++ variables-and-types.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Variables
+{
+    public static void Main()
+    {
+        int myInt = 1;
+        float myFloat = 1f;
+        bool myBoolean = true;
+        string myName = "John";
+        char myChar = 'a';
+        double myDouble = 1.75;
+        System.Console.WriteLine(myInt);
+
+
+    }
+}
