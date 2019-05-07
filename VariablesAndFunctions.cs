75ebdcd1c382146006791722da064651b80887a2
Camila Carwalho <camilacarwalho3@gmail.com>
2019-05-07 17:21:58 -0300

    Behaviour : Variables and Functions

---
+++ VariablesAndFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+    int MultiplyByTwo (int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
