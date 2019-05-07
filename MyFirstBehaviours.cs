9267b9821f07b54a7599dae2349b7e8fc43626ad
Camila Carwalho <camilacarwalho3@gmail.com>
2019-05-07 17:12:51 -0300

    Behaviour : Color change

---
+++ MyFirstBehaviours.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+public class MyFirstBehaviourScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Ação deve acontecer aqui
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+	
+}
