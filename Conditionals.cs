5bcd63f084d69434f184fabbc31d49384bc4ecc7
Camila Carwalho <camilacarwalho3@gmail.com>
2019-05-07 17:03:47 -0300

    Condicionais

---
+++ Conditionals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Conditionals{
+	public static void Main(){
+        int a = 4;
+        bool b = a == 4;
+        if (b){
+            Console.WriteLine("its truuuu");
+        }
+    }
+}
